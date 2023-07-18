b4c8cfc666465e14d5f17e5713194fd6ea742270
Location <jamesabcde277@gmail.com>
2023-07-18 19:18:34 +0800

    add api test function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+
+class Api{
+  static const String port='http://192.168.0.14:8000';
+
+  Future<String> fetchHello() async {
+    try{
+      final response = await http
+          .get(Uri.parse('$port/hello'));
+      print("response code: ${response.statusCode}");
+      if (response.statusCode == 200) {
+        return response.body;
+      } else {
+        throw Exception('Failed to load Hello');
+      }
+    }
+    catch(e){
+      print(e);
+      return '';
+    }
+  }
+
+  Future<String> register(User user) async{
+
+    try{
+      final response = await http.post(Uri.parse('$port/api/user'),body: user.toJson());
+      print("response code: ${response.statusCode}");
+      return response.body;
+    }
+    catch(e){
+      print(e);
+      return '';
+    }
+  }
+}
+class User{
+  final String uid;
+  final String email;
+  final String password;
+
+  User(this.uid, this.email, this.password);
+
+  factory User.fromJson(dynamic json){
+    return User(json['uid'] as String,
+        json['email'] as String,
+        json['password'] as String);
+  }
+  Map toJson(){
+    return {
+      'uid' : uid,
+      'email' : email,
+      'password' : password
+    };
+  }
+  @override
+  String toString() {
+    return '{ $uid, $email,$password }';
+  }
+}
+
 
 void main() {
   runApp(const MyApp());
@@ -6,22 +68,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,76 +83,89 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
+  
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  Future<String>? futureHello;
+  Future<String>? futureRegister;
+  final api=Api();
+  final user=User('James','jamesabcde277@gmail.com','123456789');
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    
+    futureHello=api.fetchHello();
+    futureRegister=api.register(user);
+    print(user.toJson());
+  }
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
+  Widget registerTest(){
+    return SizedBox(
+      width:  300,
+      height: 200,
+      child:
+      FutureBuilder<String>(
+        future: futureRegister,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Text(
+              snapshot.data!,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 20),
+            );
+          } else if (snapshot.hasError) {
+            return Text(
+              '${snapshot.error}',
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontWeight: FontWeight.normal, fontSize: 20),
+            );
+          }
 
+          // By default, show a loading spinner.
+          return const CircularProgressIndicator();
+        },
+      )
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            // SizedBox(
+            //   width: 100,
+            //   height: 100,
+            //   child:FutureBuilder<String>(
+            //     future: futureHello,
+            //     builder: (context, snapshot) {
+            //       if (snapshot.hasData) {
+            //         return Text(snapshot.data!);
+            //       } else if (snapshot.hasError) {
+            //         return Text('${snapshot.error}');
+            //       }
+            //
+            //       // By default, show a loading spinner.
+            //       return const CircularProgressIndicator();
+            //     },
+            //   ),
+            // ),
+            registerTest(),
           ],
         ),
       ),
@@ -109,7 +173,8 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+
     );
   }
 }
