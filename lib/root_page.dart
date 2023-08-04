a04aa546bc599a0b218798ce06254c0b3ede640f
Location <jamesabcde277@gmail.com>
2023-08-04 15:50:15 +0800

    rewrite user_api unit test

--- lib/root_page.dart
+++ lib/root_page.dart
@@ -3,6 +3,7 @@ import 'package:school_project/api/provider/auth_provider.dart';
 import 'package:school_project/storage/storage.dart';
 import 'package:school_project/ui/pages/home_page.dart';
 import 'package:school_project/ui/pages/login_page.dart';
+import 'package:chalkdart/chalk.dart';
 
 class RootPage extends StatefulWidget {
   const RootPage({super.key});
@@ -15,6 +16,8 @@ enum SignInState { tryingAutoSignIn, autoSignInSucceed, autoSignInFailed }
 class _RootPage extends State<RootPage> {
   SignInState signInState = SignInState.tryingAutoSignIn;
   Future<void> autoSignIn(BuildContext context) async {
+    if(signInState!=SignInState.tryingAutoSignIn) return ;
+    print(chalk.yellow('trying to auto login with locally stored account and password'));
     //try to sign in with local stored account
     final userStorage = UserStorage();
     final userApi = AuthProvider.of(context).userApi;
