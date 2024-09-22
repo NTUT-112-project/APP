c38eec7807703ed68bce7addd2f731b8b98289bf
James-Lu-none <jamesabcde277@gmail.com>
2024-09-22 10:35:49 +0800

    refactor: remove unnecessary @NonNull annotation from configureFlutterEngine method

--- android/app/src/main/kotlin/com/example/school_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/school_project/MainActivity.kt
@@ -8,7 +8,7 @@ import io.flutter.plugin.common.MethodChannel
 class MainActivity: FlutterActivity() {
     private val channel = "com.example.APP/overlay"
 
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel).setMethodCallHandler {
             call, result ->
